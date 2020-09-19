ac963c0704ece37d7fb2788e641a2cde0e1a5e25
ruining-he <71536527+ruining-he@users.noreply.github.com>
2020-09-19 10:39:42 +0800

    Add files via upload

---
+++ operation.py
@@ -0,0 +1,13 @@
+import sys
+
+
+def list_dot_mul_sum(list_a, list_b):
+    if len(list_a) != len(list_b):
+        sys.exit("Error: list_dot_mul_sum: lists have different length.")
+    return sum([list_a[i]*list_b[i] for i in range(len(list_a))])
+
+# def input_data():
+
+
+if __name__ == '__main__':
+    print("RESULT: list_dot_mul:", list_dot_mul_sum([1, 2, 3], [1, 1, 1]))
