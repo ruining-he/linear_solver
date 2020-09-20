a101f77d7d2f1ab702b56e19087d6829cacc8128
ruining-he <71536527+ruining-he@users.noreply.github.com>
2020-09-20 22:19:33 +0800

    Update operation.py
    add if_equal function. (float, representation)

--- src/operation.py
+++ src/operation.py
@@ -6,6 +6,13 @@ def list_dot_mul_sum(list_a, list_b):
         sys.exit("Error: list_dot_mul_sum: lists have different length.")
     return sum([list_a[i]*list_b[i] for i in range(len(list_a))])
 
+
+def if_equal(a, b):
+    if abs(a-b) < 0.0001:
+        return True
+    else:
+        return False
+
 # def input_data():
 
 
